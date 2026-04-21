11872a364931fd2fc2d5297984e7f4343b9057f2
realsebyake <149763501+realsebyake@users.noreply.github.com>
2026-04-21 22:49:07 +0300

    data types and structures

---
+++ Chapter 2/DataTypesAndSizes.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
+
+printf("CHAR_MAX: %d\n", CHAR_MAX);
+printf("CHAR_MIN: %d\n", CHAR_MIN);
+printf("INT_MAX: %d\n", INT_MAX);
+printf("INT_MIN: %d\n", INT_MIN);
+printf("LONG_MAX: %ld\n", LONG_MAX);
+printf("LONG_MIN: %ld\n", LONG_MIN);
+printf("SHRT_,MAX: %d\n", SHRT_MAX);
+printf("SCHAR_MAX: %d\n", SCHAR_MAX);
+printf("SCHAR_MIND: %d\n", SCHAR_MIN);
+printf("UCHAR_MAX: %d\n", UCHAR_MAX);
+printf("UINT_MAX: %u\n", UINT_MAX);
+printf("ULONG_MAX: %lu\n", ULONG_MAX);
+printf("USHRT_MAX: %d\n", USHRT_MAX);
+
+return 0;
+}
